c80a4b78ed00b9ccf90eaaff524c8ad516222483
Aldo Hernandez Corchado <aldohernandezcorchado@gmail.com>
2026-06-25 08:48:22 -0400

    Update install_R_lbraries.R

--- src/_R/install_R_lbraries.R
+++ src/_R/install_R_lbraries.R
@@ -1,3 +1,23 @@
+# =======================================================================
+#  Authors: Aldo Hernandez-Corchado, Hamed S. Najafabadi
+#
+#  Copyright 2026 Aldo Hernandez-Corchado, Hamed S. Najafabadi
+#
+#  This file is part of MAGIX.
+#
+#  MAGIX is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  MAGIX is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with MAGIX.  If not, see <http://www.gnu.org/licenses/>.
+# =======================================================================
 if (!require("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
